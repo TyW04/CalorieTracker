d32612591a3b576e919480a44c234a6c803a66db
TyW04 <tywagner04@gmail.com>
2026-06-13 15:01:50 -0600

    Bottom navbar layout present. Will likely change, but structure and appearance is generally done.

--- app/src/main/java/com/example/calorietracker/ui/MainActivity.kt
+++ app/src/main/java/com/example/calorietracker/ui/MainActivity.kt
@@ -1,48 +1,42 @@
-package com.example.calorietracker
+package com.example.calorietracker.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.example.calorietracker.R
 import com.example.calorietracker.ui.theme.CalorieTrackerTheme
-import kotlinx.coroutines.flow.WhileSubscribed
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             CalorieTrackerTheme {
                 Scaffold(
@@ -101,10 +95,57 @@ fun TopBar() {
 
 @Composable
 fun BottomNavBar() {
-    // Home Button
-    // Track Button
-    // Cook Button
-    // Progress Button
+    NavigationBar(
+        containerColor = Color(0xFFFCDFC3)
+    ) {
+        // Home Button
+        NavigationBarItem (
+            icon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.home_button_icon),
+                    contentDescription = "Home")
+            },
+            selected = true, // TODO: Replace this with something to track if it is selected
+            label = { Text("Home") },
+            onClick = {}
+        )
+        // Track Button
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.track_button_icon),
+                    contentDescription = "Track")
+            },
+            selected = false, // TODO: Replace this with something to track if it is selected
+            label = { Text("Track") },
+            onClick = {}
+        )
+        // Cook Button
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.cook_button_icon),
+                    contentDescription = "Cook"
+                )
+            },
+            selected = false, // TODO: Replace this with something to track if it is selected
+            label = { Text("Cook") },
+            onClick = {}
+        )
+        // Progress Button
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.progress_button_icon),
+                    contentDescription = "Progress"
+                )
+            },
+            selected = false,
+            label = { Text("Progress") },
+            onClick = {}
+        )
+    }
+
     return
 }
 
@@ -116,6 +157,11 @@ fun MainContent(modifier: Modifier = Modifier) {
     return
 }
 
+@Preview(
+    name = "Pixel 8",
+    showBackground = true,
+    device = "id:pixel_8"
+)
 @Preview(showBackground = true)
 @Composable
 fun CalorieTrackerPreview() {
