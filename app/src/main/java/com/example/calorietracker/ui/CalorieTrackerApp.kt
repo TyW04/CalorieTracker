19c016f4b234ba905074914c32a24da370b4caa7
TyW04 <tywagner04@gmail.com>
2026-07-23 21:57:41 -0600

    Add placeholder function so previews will work for other modules as they are built

--- app/src/main/java/com/example/calorietracker/ui/CalorieTrackerApp.kt
+++ app/src/main/java/com/example/calorietracker/ui/CalorieTrackerApp.kt
@@ -1,2 +1,9 @@
 package com.example.calorietracker.ui
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CalorieTrackerApp() {
+    Text("App Root Shell - Under Construction. Placeholder.")
+}
